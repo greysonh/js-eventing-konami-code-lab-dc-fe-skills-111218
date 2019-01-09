00acd778958370a718f201b09124800c65153e26
Greyson Harris <gharris@esri.com>
2019-01-09 01:54:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init () {
   document.body.addEventListener("keydown", function(e) {
-    const key = ()
+    const key = e.detail || e.which;
     console.log(e);
   })
 }
